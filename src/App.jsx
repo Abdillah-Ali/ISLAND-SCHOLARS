d5d966f8f01578407aea190072353a833ebe7d8b
Abdillah-Ali <abdillahx132x@gmail.com>
2025-05-29 11:18:40 +0300

    Redeploy to GitHub Pages with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import { NotificationProvider } from './contexts/NotificationContext';
